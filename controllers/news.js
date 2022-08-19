7ad4e545b7e2624a3d33c3318f69f65d42206178
Simeon Mladenov <lamerkata@abv.bg>
2022-08-19 23:25:14 +0300

    test

--- controllers/news.js
+++ controllers/news.js
@@ -61,8 +61,8 @@ export const list = async (req, res) => {
     if (startDate && endDate) {
       const minDate = moment(startDate).startOf('day').add(2, 'hours').toDate()
       const maxDate = moment(endDate).endOf('day').add(3, 'hours').toDate()
-      filter.createdAt = { $gte: minDate, $lte: maxDate }
-      filter.$or = [ { pinned: true } ]
+      // filter.createdAt = { $gte: minDate, $lte: maxDate }
+      filter.$or = [ { pinned: true }, { createdAt: { $gte: minDate, $lte: maxDate } }]
     }
 
     const secondFilter = {  }
