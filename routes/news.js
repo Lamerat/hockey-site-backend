edeff2c163056dc5ee2d0a6b35011a270e35d8c0
Simeon Mladenov <lamerkata@abv.bg>
2022-09-17 15:41:39 +0300

    Change routers

--- routes/news.js
+++ routes/news.js
@@ -4,8 +4,8 @@ import authentication from '../middleware/authentication.js'
 const router = express.Router()
 
 router.post('/create', authentication, controller.create)
-router.get('/:_id', authentication, controller.single)
-router.post('/list', authentication, controller.list)
+router.get('/:_id', controller.single)
+router.post('/list', controller.list)
 router.put('/:_id', authentication, controller.edit)
 router.delete('/:_id', authentication, controller.remove)
 
