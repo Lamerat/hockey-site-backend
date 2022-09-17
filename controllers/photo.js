adce1d45eb218a1496557676011eb07337e8495e
Simeon Mladenov <lamerkata@abv.bg>
2022-09-17 10:54:36 +0300

    Add file format validation

--- controllers/photo.js
+++ controllers/photo.js
@@ -21,10 +21,12 @@ export const upload = async (req, res) => {
     const requests = []
 
     if (Array.isArray(images)) {
-      images.forEach(x => requests.push({ data: x.data, original_name: x.name}))
+      images.forEach(x => requests.push({ data: x.data, mimetype: x.mimetype, original_name: x.name}))
     } else {
-      requests.push({ data: images.data, original_name: images.name})
+      requests.push({ data: images.data, mimetype: images.mimetype, original_name: images.name})
     }
+
+    if (requests.some(x => !x.mimetype.startsWith('image'))) throw new CError('Invalid file format', 406)
     
     const files = await Promise.all(requests.map(async file => {
       const formData = new FormData()
