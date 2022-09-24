a63fbb744fcaa30b46aca54a1ecf9f9b45f26161
Simeon Mladenov <lamerkata@abv.bg>
2022-09-24 17:22:58 +0300

    fix

--- utilities/express-helpers.js
+++ utilities/express-helpers.js
@@ -1,7 +1,8 @@
 export const errorRes = (res, error) => {
-  const code = error.code || 422
+  let code = error.code || 422
   console.log(`\x1b[31mERROR: \x1b[35m ${error.message}\x1b[0m`)
   console.log(error)
+  if (isNaN(code)) code = 409
   res.status(code > 511 ? 500 : code).json({ success: false, message: error.message }).end()
 }
 
